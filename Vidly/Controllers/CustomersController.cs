9196210be76d0be552f4d86041ce5ea4e66aea28
Notz <ggwplol5513@gmail.com>
2017-11-07 10:12:31 +0800

    Add files via upload

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "John Smith", Id = 1},
+                new Customer { Name = "Mary Williams", Id = 2},
+                new Customer { Name = "Notz", Id = 3}
+                
+            };
+
+            var viewModel = new CustomerViewModel()
+            {
+                Customer = customers
+            };
+
+            return View(viewModel);
+        }
+
+        //[Route("Customers/Detail/{id:range(1,3)}")]
+        public ActionResult Detail(int id)
+        {
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "John Smith", Id = 1},
+                new Customer { Name = "Mary Williams", Id = 2},
+                new Customer { Name = "Notz", Id = 3}
+
+            };
+
+            var viewModel = new CustomerViewModel()
+            {
+                Customer = customers
+            };
+
+            var check = GetCustomers().SingleOrDefault(c => c.Id == id);
+
+            if (check == null)
+            {
+                return HttpNotFound();
+            }
+
+
+            return View(viewModel);
+        }
+
+        private IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer { Id = 1, Name = "John Smith" },
+                new Customer { Id = 2, Name = "Mary Williams"},
+                new Customer { Id = 3, Name = "Notz"}
+            };
+        }
+    }
+}
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        /*public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer 1"},
+                new Customer { Name = "Customer 2"}
+            };
+
+            var viewModel = new RandomMovieViewModel()
+            {
+                Movie = movie,
+                Customer = customers
+            };
+
+
+            return View(viewModel);
+        }*/
+
+        /*public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }*/
+
+
+        // to movies
+        public ActionResult Index()
+        {
+            var movieList = new List<Movie>
+            {
+                new Movie { Name = "Shrek!", Id = 1},
+                new Movie { Name = "Wall-e", Id = 2}
+                
+            };
+
+            var viewModel = new MoviesViewModel()
+            {
+                movie = movieList
+            };
+
+            return View(viewModel);
+        }
+
+        /*[Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }*/
+    }
+}
